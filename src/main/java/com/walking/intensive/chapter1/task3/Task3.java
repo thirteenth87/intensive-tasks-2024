2f09f7e7c0a9621196162ab0aac2df1501bcf209
thirteenth87 <ku.gordeev@gmail.com>
2025-01-30 18:27:07 +0500

    try 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-
-        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0 )) && year > 0;
-
+        return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) && year > 0;
     }
 }
